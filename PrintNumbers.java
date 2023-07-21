4dea1db12ecc05eaf669586bd86117c931937e0b
prashanthrapolu163 <139196595+prashanthrapolu163@users.noreply.github.com>
2023-07-21 15:25:17 +0530

    Add files via upload

---
+++ PrintNumbers.java
@@ -0,0 +1,10 @@
+package collections;
+
+public class PrintNumbers {
+	public static void main(String[] args) {
+		int num = 50;
+		for(int i=0; i<=num; i++) {
+			System.out.println(i);
+		}
+	}
+}
